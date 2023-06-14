072727d3f04ac03ceca1b4110282a92bee6ede40
Adam James <atj@pulsewidth.org.uk>
2023-06-14 17:56:36 +0100

    Initial commit

---
+++ test_plugins/type_tests.py
@@ -0,0 +1,15 @@
+def test_bool(value):
+    return isinstance(value, bool)
+
+
+def test_list(value):
+    return isinstance(value, list)
+
+
+class TestModule(object):
+
+    def tests(self):
+        return {
+            'bool': test_bool,
+            'list': test_list
+        }
